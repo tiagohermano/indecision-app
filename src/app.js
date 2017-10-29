705d42aa7594906f695439c4cdfa518eaee6cb18
Tiago Hermano <tiagofhermano@gmail.com>
2017-10-29 21:01:46 -0200

    Mudança na declaração de variável 'var' para 'const'

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 console.log('app.js is running');
 
-var app = {
+const app = {
   title: 'Indecision App',
   subtitle: 'This is some info',
   options: ['one']
 }
 
-var template = (
+const template = (
   <div>
     <h1>{app.title}</h1>
     {app.subtitle && <p>{app.subtitle}</p>}
@@ -19,7 +19,7 @@ var template = (
   </div>
 );
 
-var user = {
+const user = {
   name: 'Tiago',
   location: 'Brazil',
   age: 21
@@ -33,13 +33,13 @@ function getLocation(location) {
   }
 }
 
-var template2 = (
+const template2 = (
   <div>
     <h1>{user.name ? user.name : 'Anonymous'}</h1>
     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
     {getLocation(user.location)}
   </div>
 );
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
