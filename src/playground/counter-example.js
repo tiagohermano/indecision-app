a96fa0d0669d9eb0de31ccb8bd23f9be61838088
Tiago Hermano <tiagofhermano@gmail.com>
2018-05-18 16:21:29 -0300

    Refatorado Counter App usando métodos de ciclo de vida e persistência de dados através do localStorage

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -11,24 +11,36 @@ class Counter extends React.Component {
     }
   }
 
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('count');
+      const count = JSON.parse(json);
+
+      if (count) {
+        this.setState(() => ({ count }));
+      }
+    } catch (e) {
+      // Do nothing
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.count !== this.state.count) {
+      const json = JSON.stringify(this.state.count);
+      localStorage.setItem('count', json);
+    }
+  }
+
   handleAddOne() {
-    this.setState(() => ({count: prevState.count + 1}));
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
   }
 
   handleMinusOne() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count - 1,
-      }
-    })
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
   }
 
   handleReset() {
-    this.setState(() => {
-      return {
-        count: 0,
-      }
-    })
+    this.setState(() => ({ count: 0 }));
   }
 
   render() {
