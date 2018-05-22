8206b7a844cd17de98353f7c1e8efc6a579f335e
Tiago Hermano <tiagofhermano@gmail.com>
2018-05-22 09:13:12 -0300

    instalado normalize.css e resetado estilo padrão para todos os browsers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import IndecisionApp from './components/IndecisionApp';
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => (
   <div className="header">
     <h1 className="header__title">{props.title}</h1>
-    <h2 className="header_subtitle">{props.subtitle}</h2>
+    <h2 className="header__subtitle">{props.subtitle}</h2>
   </div>
 );
 
