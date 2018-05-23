7c924be4366eafecf180b4667145a1efd4cb7564
Tiago Hermano <tiagofhermano@gmail.com>
2018-05-23 00:16:01 -0300

    Upado screenshot da aplicação

--- src/playground/es6-arrow-function.js
+++
@@ -1,9 +0,0 @@
-const multiplier = {
-  numbers: [3,5,10],
-  multiplyBy: 4,
-  multiply() {
-    return this.numbers.map((number) => number * this.multiplyBy);
-  }
-}
-
-console.log(multiplier.multiply());
